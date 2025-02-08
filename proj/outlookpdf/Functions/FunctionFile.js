2c51a9df88230c2005089435621ec781d2cbf461
tl4l0c <tl4l0c@gmail.com>
2025-02-08 12:37:12 -0800

    Principal files

---
+++ proj/outlookpdf/Functions/FunctionFile.js
@@ -0,0 +1,23 @@
+﻿// Loads the Office.js library.
+Office.onReady();
+
+// Helper function to add a status message to the notification bar.
+function statusUpdate(icon, text, event) {
+  const details = {
+    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+    icon: icon,
+    message: text,
+    persistent: false
+  };
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", details, { asyncContext: event }, asyncResult => {
+    const event = asyncResult.asyncContext;
+    event.completed();
+  });
+}
+// Displays a notification bar.
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!", event);
+}
+
+// Maps the function name specified in the manifest to its JavaScript counterpart.
+Office.actions.associate("defaultStatus", defaultStatus);
