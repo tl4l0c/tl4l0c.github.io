f96ebc1d73cec0a28073b3acd4aa0a627a456efd
tl4l0c <tl4l0c@gmail.com>
2025-02-14 11:44:49 -0800

    include jsfdf library

--- proj/outlookpdf/MessageRead.js
+++ proj/outlookpdf/MessageRead.js
@@ -14,7 +14,7 @@
         // Write message property values to the task pane
         console.log('item:');
         console.log(item);
-        $('#item-title').text('2025-02-14 11:39');
+        $('#item-title').text('2025-02-14 11:43');
         $('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
         $('#item-internetMessageId').text(item.internetMessageId);
