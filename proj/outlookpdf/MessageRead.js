1f458b4f587ddb6939cdd8c41b8339327111aacd
tl4l0c <tl4l0c@gmail.com>
2025-02-14 11:32:32 -0800

    body

--- proj/outlookpdf/MessageRead.js
+++ proj/outlookpdf/MessageRead.js
@@ -14,12 +14,20 @@
         // Write message property values to the task pane
         console.log('item:');
         console.log(item);
-        $('#item-title').text('2025-02-14 11:25');
+        $('#item-title').text('2025-02-14 11:30');
         $('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
         $('#item-internetMessageId').text(item.internetMessageId);
         $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
-        $('#item-html').html(item.html);
-        console.log(item);
+        
+        item.body.getAsync(Office.CoercionType.Html, (result) => {
+            console.log('item.body.getAsync');
+            if (result.status === Office.AsyncResultStatus.Succeeded) {
+                console.log("Contenido del correo:", result.value);
+                $('#item-html').html(result.value);
+            } else {
+                console.error("oError al obtener el cuerpo:", result.error);
+            }
+        });
     }
 })();
