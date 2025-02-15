17f59573e7c72142e2bb447c6ac768fb5458ca17
tl4l0c <tl4l0c@gmail.com>
2025-02-14 16:41:51 -0800

    wrong style property

--- proj/outlookpdf/MessageRead.js
+++ proj/outlookpdf/MessageRead.js
@@ -14,7 +14,7 @@
         // Write message property values to the task pane
         console.log('item:');
         console.log(item);
-        $('#item-title').text('2025-02-14 16:36');
+        $('#item-title').text('2025-02-14 16:40');
         //$('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
         //$('#item-internetMessageId').text(item.internetMessageId);
@@ -53,7 +53,7 @@
         console.log('generatePDF 1');
 
         const outlookHtml = `
-                <div style="width=800px;">
+                <div style="width: 800px; margin: 0 auto;">
                      ${htmlContent}
                 </div>
             `;
@@ -66,7 +66,7 @@
             x: 10,
             y: 10,
             html2canvas: {
-                //scale: 0.2,
+                scale: 0.8,
                 width: 800
             } 
         });
