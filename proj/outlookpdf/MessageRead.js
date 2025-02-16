75b6a5a2eaad2f7a2bda3721e6c12ae0281592d5
tl4l0c <tl4l0c@gmail.com>
2025-02-16 11:32:41 -0800

    Email data.

--- proj/outlookpdf/MessageRead.js
+++ proj/outlookpdf/MessageRead.js
@@ -14,11 +14,22 @@
         // Write message property values to the task pane
         console.log('item:');
         console.log(item);
-        $('#item-title').text('2025-02-15 20:29');
+        $('#item-version').text('2025.02.16.11.18');
         //$('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
         //$('#item-internetMessageId').text(item.internetMessageId);
         //$('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
+
+        let from = item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;";
+        let to = item.to.displayName + " &lt;" + item.to.emailAddress + "&gt;";
+        let cc = '';
+        let bcc = '';
+        if (item.cc.length != 0) {
+            cc = item.cc.displayName + " &lt;" + item.cc.emailAddress + "&gt;";
+        }
+        if (item.bcc.length != 0) {
+            bcc = item.bcc.displayName + " &lt;" + item.bcc.emailAddress + "&gt;";
+        }
         
         item.body.getAsync(Office.CoercionType.Html, (result) => {
             console.log('item.body.getAsync');
@@ -26,14 +37,14 @@
                 console.log('status ok');
                 console.log('body:', result.value);
                 // $('#item-html').html(result.value);
-                generatePDF(result.value, item.subject);
+                generatePDF(result.value, item.subject, from, to, cc, bcc);
             } else {
                 console.error("Error al obtener el cuerpo:", result.error);
             }
         });
     }
 
-    function generatePDF(htmlContent, subject) {
+    function generatePDF(htmlContent, subject, from, to, cc, bcc) {
         console.log('generatePDF init.');
         const { jsPDF } = window.jspdf;
 
@@ -54,6 +65,36 @@
 
         let outlookHtml = ``;
 
+        outlookHtml = `
+                <div style="width: 800px; margin: 10px auto;">
+                    <table>
+                        <tr>
+                            <td>From:</td>
+                            <td>${from}</td>
+                        </tr>
+                        <tr>
+                            <td>To:</td>
+                            <td>${to}</td>
+                        </tr>
+                        <tr>
+                            <td>cc:</td>
+                            <td>${cc}</td>
+                        </tr>
+                        <tr>
+                            <td>bcc:</td>
+                            <td>${bcc}</td>
+                        </tr>
+                         <tr>
+                            <td>Subject:</td>
+                            <td>${subject}</td>
+                        </tr>
+                    </table>
+
+                     ${htmlContent}
+                </div>
+            `;
+
+        /*
         getBase64Image("https://cdn.graph.office.net/prod/media/shared/Microsoft_Logo_White.png", function (base64Image) {
             console.log(base64Image); // Reemplaza la imagen con su versión Base64
             outlookHtml = `
@@ -63,7 +104,7 @@
                 </div>
             `;
         });
-
+        */
 
         doc.html(outlookHtml, {
             callback: function (pdf) {
