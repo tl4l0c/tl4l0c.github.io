43d738d2ab04c3b2679d81b54e97ff8383a2a832
tl4l0c <tl4l0c@gmail.com>
2025-02-15 20:11:16 -0800

    base64

--- proj/outlookpdf/MessageRead.js
+++ proj/outlookpdf/MessageRead.js
@@ -14,7 +14,7 @@
         // Write message property values to the task pane
         console.log('item:');
         console.log(item);
-        $('#item-title').text('2025-02-15 19:59');
+        $('#item-title').text('2025-02-15 20:10');
         //$('#item-id').text(item.itemId);
         $('#item-subject').text(item.subject);
         //$('#item-internetMessageId').text(item.internetMessageId);
@@ -52,37 +52,18 @@
 
         console.log('generatePDF 1');
 
-        const outlookHtml = `
+        let outlookHtml = ``;
+
+        loadImageToBase64("https://cdn.graph.office.net/prod/media/shared/Microsoft_Logo_White.png", function (base64Image) {
+            console.log(base64Image); // Reemplaza la imagen con su versión Base64
+            outlookHtml = `
                 <div style="width: 800px; margin: 10px auto;">
+                <img src="${base64Image}" alt="Red dot" />
                      ${htmlContent}
                 </div>
             `;
-
-        doc.html(outlookHtml, {
-            callback: function (pdf) {
-                console.log('generatePDF 2');
-
-                // Usar html2canvas para capturar el contenido de la página como imagen
-                html2canvas(document.body, { scale: 0.8, useCORS: true }).then(canvas => {
-
-
-                    const imgData = canvas.toDataURL("image/png");
-                    doc.addImage(imgData, "PNG", 10, 10);
-                    doc.addImage("https://cdn.graph.office.net/prod/media/shared/Microsoft_Logo_White.png", "PNG", 10, 50, 100, 50); // Imagen agregada manualmente
-
-                    pdf.save("Email." + formatFileName(subject) + ".pdf");
-
-                });
-            },
-            x: 10,
-            y: 10,
-            html2canvas: {
-                scale: 0.5,
-                useCORS: true
-            }
         });
 
-            /*
         doc.html(outlookHtml, {
             callback: function (pdf) {
                 console.log('generatePDF 2');
@@ -96,8 +77,24 @@
                 useCORS: true
             } 
         });
-        */
         console.log('generatePDF end.');
+    }
+
+    function loadImageToBase64(url, callback) {
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.onload = function () {
+            const canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            callback(canvas.toDataURL("image/png"));
+        };
+        img.onerror = function () {
+            console.error("No se pudo cargar la imagen:", url);
+        };
+        img.src = url;
     }
 
     function formatFileName(subject) {
